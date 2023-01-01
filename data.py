93d14a0de05f9168132667e6a1725e11d9ca38ed
cemterakye <cem.terakye@gmail.com>
2023-01-01 21:22:04 +0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,7 @@
+import requests
+import json
+
+response = requests.get(url = "https://opentdb.com/api.php?amount=10&type=boolean")
+response.raise_for_status()
+question_data = response.json()["results"]
+
